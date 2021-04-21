a9026812c7fe094e1e9fc31c28874a3c248a37cf
aleksei.potapov <aleksei.potapov@doclerholding.com>
2021-04-21 16:36:42 +0200

    implement movie recommendation

--- src/main/java/com/docler/examles/Recommendations.java
+++ src/main/java/com/docler/examles/Recommendations.java
@@ -3,8 +3,7 @@ package com.docler.examles;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /*
@@ -58,24 +57,69 @@ public class Recommendations {
     );
 
 
-    public static List<PersonScore> topMarchers(String person, int topN, String simularity) {
-        List<PersonScore> scores =
+    public static List<KeyScore> topMarchers(String person, int topN, String similarity) {
+        List<KeyScore> scores =
                 recommendations.keySet().stream().filter(personFromDataSet -> !personFromDataSet.equals(person))
                         .map(personFromDataSet -> {
-                            if ("Euclidean".equals(simularity)) {
-                                return new PersonScore(personFromDataSet, EuclideanDistanceScore.getSimilarityDistance(recommendations, person, personFromDataSet));
-
+                            if ("Euclidean".equals(similarity)) {
+                                return new KeyScore(personFromDataSet, EuclideanDistanceScore.getSimilarityDistance(recommendations, person, personFromDataSet));
                             } else {
-                                return new PersonScore(personFromDataSet, PearsonCorrelationScore.getSimilarityDistance(recommendations, person, personFromDataSet));
+                                return new KeyScore(personFromDataSet, PearsonCorrelationScore.getSimilarityDistance(recommendations, person, personFromDataSet));
                             }
                         }).sorted((e1, e2) -> e2.getScore().compareTo(e1.getScore())).collect(Collectors.toList());
 
         return scores.subList(0, topN);
     }
 
+    public static List<KeyScore> getRecommendations(String person, String similarityAlg) {
+        Map<String, Double> totals = new HashMap<>();
+        Map<String, Double> simSum = new HashMap<>();
+        for (String personFromDataSet : recommendations.keySet()) {
+            //don't compare me to myself
+            if (personFromDataSet.equals(person)) {
+                continue;
+            }
+
+            double sim;
+            if ("Euclidean".equals(similarityAlg)) {
+                sim = EuclideanDistanceScore.getSimilarityDistance(recommendations, person, personFromDataSet);
+            } else {
+                sim = PearsonCorrelationScore.getSimilarityDistance(recommendations, person, personFromDataSet);
+            }
+            //ignore scores of zero or lower
+            if (sim <= 0) {
+                continue;
+            }
+
+            Map<String, Double> otherPersonScore = recommendations.get(personFromDataSet);
+            for (String movie : otherPersonScore.keySet()) {
+                //only score movies person hasn't seen yet
+                if (!recommendations.get(person).containsKey(movie) || recommendations.get(person).get(movie) == 0.0) {
+                    continue;
+                }
+                if (totals.containsKey(movie)) {
+                    totals.put(movie, recommendations.get(personFromDataSet).get(movie) * sim + totals.get(movie));
+                    simSum.put(movie, sim + simSum.get(movie));
+                } else {
+                    totals.put(movie, recommendations.get(personFromDataSet).get(movie) * sim);
+                    simSum.put(movie, sim);
+                }
+            }
+        }
+        //Create normalized list
+        List<KeyScore> rankings = new ArrayList<>();
+        for (String movie : totals.keySet()) {
+            rankings.add(new KeyScore(movie, totals.get(movie) / simSum.get(movie)));
+        }
+        rankings.sort((o1, o2) -> o2.getScore().compareTo(o1.getScore()));
+        return rankings;
+    }
+
     public static void main(String[] args) {
         System.out.println("=========================PearsonCorrelationScore=========================");
         System.out.println("\"Toby\" top 3 recomenders: " + topMarchers("Toby", 3, "Pearson"));
+        System.out.println("\"Toby\" Recommendations with Pearson algorithm: " + getRecommendations("Toby", "Pearson"));
+        System.out.println("\"Toby\" Recommendations with Euclidean algorithm: " + getRecommendations("Toby", "Euclidean"));
         System.out.println("=========================PearsonCorrelationScore=========================");
 
     }
@@ -83,14 +127,14 @@ public class Recommendations {
     @Data
     @AllArgsConstructor
     static
-    class PersonScore {
-        String person;
+    class KeyScore {
+        String key;
         Double score;
 
         @Override
         public String toString() {
             return "(" +
-                    person + '\'' +
+                    key + '\'' +
                     ", " + score +
                     ')';
         }
